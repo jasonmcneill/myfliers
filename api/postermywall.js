bdd569b74b83abd37413bfdb972099031fb98adf
Jason McNeill <vrtjason@gmail.com>
2025-10-29 17:26:14 -0700

    Anticipate query parameters

--- api/postermywall.js
+++ api/postermywall.js
@@ -1,5 +1,6 @@
 exports.GET = (req, res) => {
-  console.log(req.query);
+  const code = req.query.code;
+  const state = req.query.state;
 
   return res.status(200).send({ msg: "ok", msgType: "success" });
 }
