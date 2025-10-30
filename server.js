493fef3b4f9786a11cd69bad3218f64fa86c91f9
Jason McNeill <vrtjason@gmail.com>
2025-10-29 22:44:51 -0700

    Add dependencies

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const PORT = 3001;
-
 const bodyParser = require("body-parser");
 
 app.use(express.static("."));
