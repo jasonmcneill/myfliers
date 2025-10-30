b934fa44da473a6760b4f108cdd04a38a0a48ce8
Jason McNeill <vrtjason@gmail.com>
2025-10-30 11:11:45 -0700

    Add TODOs

--- api/postermywall.js
+++ api/postermywall.js
@@ -2,5 +2,13 @@ exports.GET = (req, res) => {
   const code = req.query.code;
   const state = req.query.state;
 
+  // URL of this GET route is "/api/postermywall"
+
+  // TODO:  check database for validity of state
+  // TODO:  exchange code for JWT
+  // TODO:  store JWT in database (will be a perpetual refresh token and a short-lived access token)
+  // TODO:  handle errors
+  // TODO:  redirect user
+
   return res.status(200).send({ msg: "ok", msgType: "success" });
 }
