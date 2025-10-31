00000bf7d32c992647fade6dd9d4a21d446439cb
Jason McNeill <vrtjason@gmail.com>
2025-10-30 22:09:25 -0700

    Debug

--- api/postermywall.js
+++ api/postermywall.js
@@ -1,41 +1,24 @@
 exports.GET = async (req, res) => {
-  const code = req.query.code;
-  const state = req.query.state;
+  const code = req.query.code ? req.query.code : '';
+  const state = req.query.state ? req.query.state : '';
+  const scope = req.query.scope ? req.query.scope : '';
 
-  return res.status(200).send(req.query);
+  const base64Credentials = Buffer.from(`${process.env.POSTERMYWALL_KEY}:${process.env.POSTERMYWALL_SECRET}`).toString('base64');
 
-  // URL of this GET route is "/api/postermywall"
-
-  // TODO:  check database for validity of state
-  // TODO:  exchange code for JWT
-  const tokenResponse = await fetch('https://api.postermywall.com/v1/oauth/token', {
+  fetch('https://api.postermywall.com/v1/oauth/token', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      redirect_uri: 'https://myfliers.com/api/postermywall',
-      code: code,
-      grant_type: 'authorization_code',
-      client_id: process.env.POSTERMYWALL_KEY,
-    }),
-    authorization: `Basic ${Buffer.from(`${process.env.POSTERMYWALL_KEY}:${process.env.POSTERMYWALL_SECRET}`).toString('base64')}`
-  });
-
-  if (!tokenResponse.ok) {
-    const errorText = await tokenResponse.text();
-    console.log(errorText);
-    return res.redirect(`https://myfliers.com/?error=token_exchange_failed&state=${state}`);
-  }
-
-  const tokenData = await tokenResponse.json();
-  const buffer = Buffer.from(tokenData);
-  const base64String = buffer.toString('base64');
-  console.log('Access token data:', tokenData);
-
-  // TODO:  store JWT in database (will be a perpetual refresh token and a short-lived access token)
-  // TODO:  handle errors
-  // TODO:  redirect user
-  const redirectUrl = `https://myfliers.com/?userid=1&state=${state}&token=${base64String}`;
-  return res.redirect(redirectUrl);
+    headers: new Headers({
+      content_type: 'application/x-www-form-urlencoded',
+      authorization: `Basic ${base64Credentials}`
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('PosterMyWall OAuth Token Response:', data);
+      res.redirect(`https://myfliers.com/?pmw_access_token=${data.access_token}`);
+    })
+    .catch(error => {
+      console.error('Error fetching PosterMyWall OAuth token:', error);
+      res.status(500).send('Error during PosterMyWall OAuth process');
+    });
 }
