c8f490746ccd7cdfc3466ee1c4f2e241ea6d615a
Jason McNeill <vrtjason@gmail.com>
2025-10-30 17:27:06 -0700

    Try a thing

--- api/postermywall.js
+++ api/postermywall.js
@@ -1,4 +1,4 @@
-exports.GET = (req, res) => {
+exports.GET = async (req, res) => {
   const code = req.query.code;
   const state = req.query.state;
 
@@ -6,9 +6,33 @@ exports.GET = (req, res) => {
 
   // TODO:  check database for validity of state
   // TODO:  exchange code for JWT
+  const tokenResponse = await fetch('https://www.postermywall.com/api/oauth/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: process.env.POSTERMYWALL_KEY,
+      client_secret: process.env.POSTERMYWALL_SECRET,
+      redirect_uri: 'https://myfliers.com/api/postermywall',
+      grant_type: 'authorization_code',
+      code: code
+    }),
+  });
+
+  if (!tokenResponse.ok) {
+    const errorText = await tokenResponse.text();
+    throw new Error(`PosterMyWall token request failed: ${errorText}`);
+  }
+
+  const tokenData = await tokenResponse.json();
+  const buffer = Buffer.from(tokenData);
+  const base64String = buffer.toString('base64');
+  console.log('Access token data:', tokenData);
+
   // TODO:  store JWT in database (will be a perpetual refresh token and a short-lived access token)
   // TODO:  handle errors
   // TODO:  redirect user
 
-  return res.redirect(`/?userid=1&state=${state}`);
+  return res.redirect(`${process.env.FRONTEND_URL}/?userid=1&state=${state}&token=${base64String}`);
 }
