0a1629a98f8ed1b9418b5d820ebee8c97fe5848d
Matthew Song <matthewsong18@gmail.com>
2025-11-19 00:53:52 -0700

    feat(site): auto-validates usd domain

--- core-api/src/features/sites/domain/site.service.ts
+++ core-api/src/features/sites/domain/site.service.ts
@@ -5,11 +5,20 @@ export class SiteService {
   constructor(private readonly repo: SiteRepository) { }
 
   async registerSite(input: CreateSiteInput): Promise<Site> {
+    const status = this.validateUsdDomain(input.adminEmail);
+
     const repoInput: RepoCreateSiteInput = {
       ...input,
-      status: SiteStatus.Pending,
+      status,
     };
 
     return await this.repo.create(repoInput);
+  }
+
+  private validateUsdDomain(adminEmail: string): SiteStatus {
+    if (adminEmail.endsWith('@usd21.org')) {
+      return SiteStatus.Active;
+    }
+    return SiteStatus.Pending
   }
 }
