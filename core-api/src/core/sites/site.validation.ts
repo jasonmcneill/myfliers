68d5a55d2ad7caef0e6fec22de2df0c814e443db
Matthew Song <matthewsong18@gmail.com>
2025-11-26 01:22:09 -0500

    feat: site validation rejects non-valid public key

--- core-api/src/core/sites/site.validation.ts
+++ core-api/src/core/sites/site.validation.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
 import { CreateSiteInput } from "./site.types.ts";
+import { isValidPublicKey } from "../common/crypto.ts";
 
 export const CreateSiteInputSchema = z.object({
   siteName: z.string(),
   siteUrl: z.url(),
   adminEmail: z.email(),
-  publicKey: z.string(), // TODO: Figure out how to validate publicKey
+  publicKey: z.string().refine(
+    (key) => isValidPublicKey(key),
+    "The provided string is not a valid public key",
+  ),
   adapterMetadata: z.record(z.string(), z.unknown()).optional(),
 }) satisfies z.Schema<CreateSiteInput>;
