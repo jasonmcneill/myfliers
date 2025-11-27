2989b4eb4c3ec21b7d29932baacdf0f711962056
Matthew Song <matthewsong18@gmail.com>
2025-11-26 23:45:51 -0500

    fix: http response is malformed

--- core-api/src/interfaces/http/middleware/error.middleware.ts
+++ core-api/src/interfaces/http/middleware/error.middleware.ts
@@ -3,6 +3,7 @@ import { ZodError } from "zod";
 import {
   SiteAlreadyExistsError,
   SiteError,
+  SiteInputError,
 } from "../../../core/sites/site.error.ts";
 
 export const globalErrorHandler = (
@@ -12,15 +13,14 @@ export const globalErrorHandler = (
   _next: NextFunction,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({
-      message: `Bad Request: ${err.message}`,
-    });
+    zodErrorHandler(err, res);
+    return res;
   }
   if (err instanceof SiteError) {
     siteErrorHandler(err, res);
+    return res;
   }
 
-  console.error("[System Error]", err);
   return res.status(500).json({
     message: "Internal Server Error",
   });
@@ -37,5 +37,30 @@ const siteErrorHandler = (err: Error, res: Response) => {
   return res.status(400).json({
     error: err.name,
     message: err.message,
+  });
+};
+
+const zodErrorHandler = (err: ZodError, res: Response) => {
+  const details = err.issues.map((issue) => {
+    const message = issue.message;
+
+    const entry: string | undefined = Object.entries(SiteInputError)
+      .find((
+        [_key, value],
+      ) => value === message)
+      ?.[0];
+
+    const customCode = entry;
+
+    return {
+      field: issue.path.join("."),
+      message: issue.message,
+      code: customCode || issue.code,
+    };
+  });
+
+  return res.status(400).json({
+    message: "The request data is invalid.",
+    details: details,
   });
 };
